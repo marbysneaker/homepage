ed416696740e6f4a70cae63b3d76e19d2d5081da
marby <marbysneaker@gmail.com>
2019-11-13 03:28:00 -0800

    Modified build.py

--- build.py
+++ build.py
@@ -1,61 +1,47 @@
-
 pages = [{
-    "filename": "contents/about.html",
-    "output": "docs/about.html",
-    "title": "About Me"},
-    {"filename": "contents/projects.html",
-    "output": "docs/projects.html",
-    "title": "My Projects"},
-    {"filename": "contents/index.html",
-    "output": "docs/index.html",
-    "title": "Blog"}]
+        "filename": "contents/about.html",
+        "output": "docs/about.html",
+        "title": "About Me"},
+        {"filename": "contents/projects.html",
+        "output": "docs/projects.html",
+        "title": "My Projects"},
+        {"filename": "contents/index.html",
+        "output": "docs/index.html",
+        "title": "Blog"}]
 
+# combing contents and template
+def add_content():
+    new_content = []
+    for page in pages:        
+        template = open("templates/base.html").read()
+        content = open(page['filename']).read()
+        final_projects = template.replace("{{content}}", content)
+        new_content.append(final_projects)
+    return new_content
+
+#writing the new file
 def main():
-    for page in pages:
-        if page['title']== 'About Me':
-            template = open("templates/base.html").read()       
-            content = open(page['filename']).read()
-            final_about = template.replace("{{content}}", content)
-            final_about = final_about.replace("{{title}}", "Resume  / About page")
-        return open(page['output'],"w+").write(final_about,)
-   
-def projects():
-    for page in pages:
-        if page['title']== "My Projects":
-            template = open("templates/base.html").read()
-            content = open(page['filename']).read()
-            final_projects = template.replace("{{content}}", content)
-            final_projects = final_projects.replace("{{tilte}}", "Resume  / Projects page")
-            return open(page['output'],"w+").write(final_projects)
+    finals = add_title()
+    for i in range(0,3):
+        open(pages[i]['output'],"w+").write(finals[i])
 
-def blog():
-    for page in pages:
-        if page['title']== "Blog":
-            template = open("templates/base.html").read()
-            content = open(page['filename']).read()
-            final_blog = template.replace("{{content}}", content)
-            final_blog = final_blog.replace("{{tilte}}", "Resume  / Blog page")
-            return open(page['output'],"w+").write(final_blog)
-        
-
+    
             
 
+#changing title
+def add_title():
+    final = []
+    new_pages= add_content()
+    titles = ["Resume / About page","Resume / Projects page","Resume  / Blog page"]
+    for i in range(0,3):
+        title_added = new_pages[i].replace("{{title}}",titles[i])
+        final.append(title_added)
+        
+    return final
+
+        
 if __name__ == "__main__":
+    add_content()
+    add_title()
     main()
-    projects()
-    blog()
 
-
-
-    # about = open("contents/about.html").read()
-    # index = open("contents/index.html").read()
-    # projects = open("contents/projects.html").read()
-    # bottom = open("templates/bottom.html").read()
-    # top = open("templates/top.html").read()
-
-    # new_about = top + about + bottom
-    # open("docs/about.html","w").write(new_about)
-    # new_index = top + index + bottom
-    # open("docs/index.html","w").write(new_index)
-    # new_projects = top + projects + bottom
-    # open("docs/projects.html","w").write(new_projects)
